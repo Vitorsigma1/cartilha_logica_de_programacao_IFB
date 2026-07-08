402be1c9b7efdfd7e3ed0c14a992e88b5c227a96
Vitorsigma1 <vitorbuono@yahoo.com.br>
2026-07-08 20:43:19 -0300

    feat: adiciona exercicio 01 pág46 vs_code.

---
+++ vs_code_pag46_exerc_13.py
@@ -0,0 +1,9 @@
+# Escreva um algoritmo que leia dois números, calcule o produto entre eles, mostre o resultado e os 
+# números recebidos.
+
+primeiro_numero = float(input("Digite o primeiro número: "))
+segundo_numero = float(input("Digite o segundo número: "))
+resultado = primeiro_numero * segundo_numero
+
+print("O resultado da multilicação é:", resultado) 
+print("O primeiro número é:", primeiro_numero, "e o segundo número é:", segundo_numero)
